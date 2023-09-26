c5c0dc04a6239f3eac98bd7502ee48fc58bbaf83
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-09-26 14:47:16 +0100

    changed meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,27 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter, Space_Grotesk } from "next/font/google";
 
 import type { Metadata } from "next";
+import "./globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
 	title: "Dev Overflow",
 	description: "A place for developers to share their knowledge",
+	icons: {
+		icon: "/assets/images/site-logo.svg",
+	},
 };
+
+const inter = Inter({
+	subsets: ["latin", "latin-ext"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	variable: "--font-inter",
+});
+
+const spaceGrotesk = Space_Grotesk({
+	subsets: ["latin", "latin-ext"],
+	weight: ["300", "400", "500", "600", "700"],
+	variable: "--font-spaceGrotesk",
+});
 
 export default function RootLayout({
 	children,
@@ -14,9 +30,19 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<ClerkProvider>
+		<ClerkProvider
+			appearance={{
+				elements: {
+					formButtonPrimary: "primary-gradient",
+					footerActionLink: "primary-text-gradient hover:text-primary-500",
+				},
+			}}
+		>
 			<html lang="en">
-				<body>{children}</body>
+				<body className={`${inter.variable} ${spaceGrotesk.variable}`}>
+					<h1 className="h1-bold">This is a peice of code</h1>
+					{children}
+				</body>
 			</html>
 		</ClerkProvider>
 	);
