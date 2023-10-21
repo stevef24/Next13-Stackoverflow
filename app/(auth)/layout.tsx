fcf19e52acb3cc7f69761dc338cb856abc9bcc7d
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-10-21 18:05:19 +0100

    added assets

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+	return <main className="flex-center min-h-screen w-full  ">{children}</main>;
+};
+
+export default Layout;
---
+++ components/shared/Theme.tsx
@@ -0,0 +1,44 @@
+"use client";
+import React from "react";
+
+import { useTheme } from "@/context/ThemeProvider";
+import {
+	Menubar,
+	MenubarContent,
+	MenubarItem,
+	MenubarMenu,
+	MenubarSeparator,
+	MenubarShortcut,
+	MenubarTrigger,
+} from "@/components/ui/menubar";
+import Image from "next/image";
+
+const Theme = () => {
+	const { theme, setTheme } = useTheme();
+	return (
+		<Menubar className="relative border-none bg-transparent shadow-none">
+			<MenubarMenu>
+				<MenubarTrigger className="focus:bg-light-900 data-[state=open]:bg-light-900 dark:focus:bg-dark-200 dark:data-[state=open]:bg-dark-200">
+					{theme === "light" ? (
+						<Image
+							src="/assets/icons/sun.svg"
+							alt="sun"
+							width={20}
+							height={20}
+						/>
+					) : (
+						<Image
+							src="/assets/icons/moon.svg"
+							alt="mooon"
+							width={20}
+							height={20}
+						/>
+					)}
+				</MenubarTrigger>
+				<MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300"></MenubarContent>
+			</MenubarMenu>
+		</Menubar>
+	);
+};
+
+export default Theme;
