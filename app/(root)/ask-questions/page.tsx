7f95b75bdcf77aaa59d943b92882860592f6f46e
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-11-17 20:43:52 +0000

    Refactor ask-questions page and update sidebar link

--- app/(root)/ask-questions/page.tsx
+++ app/(root)/ask-questions/page.tsx
@@ -1,5 +1,14 @@
-const AskQuestions = () => {
-	return <div>Ask-questions</div>;
+import Question from "@/components/Forms/Question";
+
+const Page = () => {
+	return (
+		<div>
+			<h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+			<div className="mt-9">
+				<Question />
+			</div>
+		</div>
+	);
 };
 
-export default AskQuestions;
+export default Page;
