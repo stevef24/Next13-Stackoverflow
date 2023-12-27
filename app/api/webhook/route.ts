652ea2d293005dcc828a40dcd22a63457ee22d56
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-12-27 17:33:22 +0000

    Refactor webhook route and user action

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,16 +1,14 @@
-/* eslint-disable */
-
+/* eslint-disable camelcase */
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { createUser, updateUser, deleteUser } from "@/lib/actions/user.action";
+import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
 	// You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
 	const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
 
-	//TODO: add webhook secret to .env
 	if (!WEBHOOK_SECRET) {
 		throw new Error(
 			"Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
@@ -34,7 +32,7 @@ export async function POST(req: Request) {
 	const payload = await req.json();
 	const body = JSON.stringify(payload);
 
-	// Create a new Svix instance with your secret.
+	// Create a new SVIX instance with your secret.
 	const wh = new Webhook(WEBHOOK_SECRET);
 
 	let evt: WebhookEvent;
@@ -53,47 +51,44 @@ export async function POST(req: Request) {
 		});
 	}
 
-	// Get the ID and type
 	const eventType = evt.type;
+
 	console.log({ eventType });
+
 	if (eventType === "user.created") {
 		const { id, email_addresses, image_url, username, first_name, last_name } =
 			evt.data;
-
-		console.log(
-			id,
-			email_addresses,
-			image_url,
-			username,
-			first_name,
-			last_name
-		);
-		//create a user in the database
+		console.log(evt.data);
+		// Create a new user in your database
 		const mongoUser = await createUser({
 			clerkId: id,
-			picture: image_url,
-			username: username!,
 			name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
+			username: username!,
 			email: email_addresses[0].email_address,
+			picture: image_url,
 		});
-		console.log(mongoUser);
+
+		console.log({ mongoUser });
 		return NextResponse.json({ message: "OK", user: mongoUser });
 	}
 
-	console.log({ eventType });
 	if (eventType === "user.updated") {
 		const { id, email_addresses, image_url, username, first_name, last_name } =
 			evt.data;
+
+		// Create a new user in your database
 		const mongoUser = await updateUser({
 			clerkId: id,
 			updateData: {
-				picture: image_url,
-				username: username!,
 				name: `${first_name}${last_name ? ` ${last_name}` : ""}`,
+				username: username!,
 				email: email_addresses[0].email_address,
+				picture: image_url,
 			},
 			path: `/profile/${id}`,
 		});
+		console.log({ mongoUser });
+		return NextResponse.json({ message: "OK", user: mongoUser });
 	}
 
 	if (eventType === "user.deleted") {
@@ -103,8 +98,8 @@ export async function POST(req: Request) {
 			clerkId: id!,
 		});
 
-		return NextResponse.json({ message: "OK", user: deleteUser });
+		return NextResponse.json({ message: "OK", user: deletedUser });
 	}
 
-	return new Response("", { status: 200 });
+	return new Response("", { status: 201 });
 }
