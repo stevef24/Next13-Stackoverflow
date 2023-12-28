8415dc447d1e1ed519700d76d4e8f9923bae173e
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2023-12-28 18:48:26 +0000

    Add getAllUsers function to retrieve all users

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,11 +5,26 @@ import { connectToDatabase } from "../mongoose";
 import {
 	CreateUserParams,
 	DeleteUserParams,
+	GetAllUsersParams,
 	GetUserByIdParams,
 	UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import QuestionModel from "@/database/question.model";
+
+export async function getAllUsers(params: GetAllUsersParams) {
+	try {
+		connectToDatabase();
+
+		const users = await UserModel.find({});
+		if (!users) throw new Error("No users found");
+
+		return { users };
+	} catch (error) {
+		console.log(error);
+		throw error;
+	}
+}
 
 export async function getUserById(params: GetUserByIdParams) {
 	try {
