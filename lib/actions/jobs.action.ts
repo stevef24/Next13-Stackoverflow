7d6c283220e6adc03d8f4dac4505532c20140bb3
Stav Fernandes <stevefernandes24@hotmail.co.uk>
2024-02-14 08:44:39 +0000

    Refactor getJobsList function to use environment variable for API key

--- lib/actions/jobs.action.ts
+++ lib/actions/jobs.action.ts
@@ -1,19 +1,20 @@
 import { JobFilterParams } from "./shared.types";
 
 export async function getJobsList({ query, page, filter }: JobFilterParams) {
+	const api_key: string = process.env.NEXT_PUBLIC_RAPID_API_KEY || "";
 	try {
 		const url = `https://jsearch.p.rapidapi.com/search?query=${query}&page=${page}&num_pages=1`;
-		console.log(url);
 		const options = {
 			method: "GET",
 			headers: {
-				"X-RapidAPI-Key": "6b4f55fc0cmsh1fcf93fb6de6e0ep132b23jsn36ca8943529e",
+				"X-RapidAPI-Key": api_key,
 				"X-RapidAPI-Host": "jsearch.p.rapidapi.com",
 			},
 		};
-
+		// "6b4f55fc0cmsh1fcf93fb6de6e0ep132b23jsn36ca8943529e"
 		const response = await fetch(url, options);
 		const results = await response.json();
+		console.log(results);
 		return results;
 	} catch (error) {
 		throw new Error("No jobs found");
